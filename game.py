9af4941925f1bda4a3e4ecdfb9f29380ac7d5e91
Kim Ngo <kngo@nd.edu>
2015-04-30 10:29:56 -0400

    Began implementing client-server connection and integrating into gameplay (commented out)

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 # PyGame + Twisted final project
 # April 29, 2015
 
+#from game_client import *
 import pygame
 import sys
 import math
@@ -23,8 +24,8 @@ class Apple(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.image_eaten = pygame.image.load("images/apple_blank.png")
 
-		self.rect.centerx = randint(0+self.rect.width, GAME_WIDTH-self.rect.width)
-		self.rect.centery = randint(0+self.rect.height, GAME_WIDTH-self.rect.height)
+		self.rect.centerx = randint(0+self.rect.width*2, GAME_WIDTH-self.rect.width*2)
+		self.rect.centery = randint(0+self.rect.height*2, GAME_HEIGHT-self.rect.height*2)
 
 		print self.rect.centerx, self.rect.centery
 
@@ -32,13 +33,26 @@ class Apple(pygame.sprite.Sprite):
 
 	def tick(self):
 		if self.eaten:
-			self.rect.centerx = randint(0+self.rect.width, GAME_WIDTH-self.rect.width)
-			self.rect.centery = randint(0+self.rect.height, GAME_WIDTH-self.rect.height)
+			self.rect.centerx = randint(0+self.rect.width*2, GAME_WIDTH-self.rect.width*2)
+			self.rect.centery = randint(0+self.rect.height*2, GAME_HEIGHT-self.rect.height*2)
 			self.eaten = 0
 
+class Snake(pygame.sprite.Sprite):
+	def __init__(self,gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.gs = gs
+		self.image = pygame.image.load("images/snake_blue.png")
+		self.rect = self.image.get_rect()
+		self.rect.centerx = 960/3
+		self.rect.centery = 640/2
+		
+		self.length = 1
+
 class GameSpace:
-	def main(self):
+	def main(self):#, factory):
 		pygame.init()
+
+		#self.factory = factory
 
 		self.size = self.width, self.height = GAME_WIDTH, GAME_HEIGHT
 		self.black = 0, 0, 0, 0
@@ -49,10 +63,59 @@ class GameSpace:
 		#self.player1 = Player(self)
 		#self.player2 = Player(self)
 		self.apple = Apple(self)
+		self.player1 = Snake(self)
+		#self.pos.x = self.player1.rect.centerx
+		#self.pos.y = self.player1.rect.centery
+		#self.player2 = Snake(self)
+		#self.player2.rect.centerx *= 2
 
 		# Game loop
 		while 1:
 			self.clock.tick(60)
+
+			for event in pygame.event.get():
+				if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+
+			key = pygame.key.get_pressed()
+			if key[pygame.K_UP]:
+				if self.player1.rect.top > 0:
+					self.player1.rect.centery -= 5
+				else:
+					self.player1.rect.centery = self.height
+			if key[pygame.K_DOWN]:
+				if self.player1.rect.bottom < self.height:
+					self.player1.rect.centery += 5
+				else:
+					self.player1.rect.centery = 0
+			if key[pygame.K_LEFT]:
+				if self.player1.rect.left > 0:
+					self.player1.rect.centerx -= 5
+				else:
+					self.player1.rect.centerx = self.width
+			if key[pygame.K_RIGHT]:
+				if self.player1.rect.right < self.width:
+					self.player1.rect.centerx += 5
+				else:
+					self.player1.rect.centerx = 0
+
+			#self.pos.x = self.player1.rect.centerx
+			#self.pos.y = self.player1.rect.centery
+
+			# Check collision
+				# Snake & snake
+				# Snake and itself
+			# Collision: snake and apple
+			if pygame.sprite.collide_rect(self.apple, self.player1):
+				self.apple.eaten = 1
+				self.player1.length += 1
+				#self.factory.connections["server"].transport.write("apple")
+			
+			#self.factory.connections["server"].transport.write(self.pos)
+			#if (pos2 = self.factory.connections["server"].dataReceived()):
+			#	self.player2.rect.centerx = pos2.x
+			#	self.player2.rect.centery = pos2.y
 
 			#self.player1.tick()
 			#self.player2.tick()
@@ -60,19 +123,18 @@ class GameSpace:
 
 			self.screen.fill(self.black)
 			
-			# Check collision
-				# Snake & snake
-				# Snake and wall
-				# Snake and apple
-
 			#self.screen.blit(self.player1.image, self.player1.rect)
 			#self.screen.blit(self.player2.image, self.player2.rect)
 			self.screen.blit(self.apple.image, self.apple.rect)
+			self.screen.blit(self.player1.image, self.player1.rect)
+			#self.screen.blit(self.player2.image, self.player2.rect)
 
 			pygame.display.flip() # double buffer animation system
 
 if __name__ == '__main__':
+	#factory = ClientConnFactory()
+	#reactor.connectTCP(HOST, PORT, factory)
 	gs = GameSpace()
-	gs.main()
+	gs.main()#factory)
 
 
--- game_server.py
+++ game_server.py
@@ -4,6 +4,10 @@ from twisted.internet.protocol import Protocol
 
 CLIENTPORT1 = 9082
 CLIENTPORT2 = 9083
+GAME_WIDTH = 640
+GAME_HEIGHT = 960
+SNAKE_WIDTH = 15*2
+SNAKE_HEIGHT = 15*2
 
 '''
 HOST = 'student02.cse.nd.edu'
@@ -26,44 +30,82 @@ class Server():
 		self.client2 = 0
 
 	def listen(self):
-		reactor.listenTCP(CLIENTPORT1, ClientConnFactory(self.factory.connections, self.client1, self.client2))
-		reactor.listenTCP(CLIENTPORT2, ClientConnFactory(self.factory.connections, self.client1, self.client2))
+		reactor.listenTCP(CLIENTPORT1, ClientConnFactory1(self.connections, self.client1))
+		reactor.listenTCP(CLIENTPORT2, ClientConnFactory2(self.connections, self.client2))
 
-
-
-class ClientConnFactory(Factory):
-	def __init__(self, connections, client1, client2):
+class ClientConnFactory1(Factory):
+	def __init__(self, connections, client1):
 		self.connections = connections
 		self.client1 = client1
-		self.client2 = client2
 	def buildProtocol(self, addr):
-		return ClientConnProtocol(self)
+		return ClientConnProtocol1(self)
 
-class ClientConnProtocol(Protocol):
+
+class ClientConnProtocol1(Protocol):
 	def __init__(self, factory):
 		self.factory = factory
 
 	def connectionMade(self):
-		if not self.client1:
-			self.client1 = 1
-			self.factory.connections["client1"] = self
-		else:
-			self.client2 = 1
-			self.factory.connections["client2"] = self
-		self.factory.connections["command"].transport.write("Connected to server")
+		self.factory.client1 = 1
+		self.factory.connections["client1"] = self
+		self.factory.connections[self] = "client1"
+		self.factory.connections["client1"].transport.write("Player 1 has entered the game.\nWaiting for player 2...")
 		
 	def connectionLost(self, reason):
-		if self == self.factory.connections["client1"]:
-			del self.factory.connections["client1"]
-		else:
-			del self.factory.connections["client2"]
-		print "Lost connection:", reason
+		del self.factory.connections["client1"]
+		print "Player 1 has left the game."
+		#print "Lost connection:", reason
 
-	#def dataReceived(self, data):
-	# send user key strokes to game server	
+	def dataReceived(self, data):
+		if data == "apple":
+			x = randint(0+SNAKE_WIDTH, GAME_WIDTH-SNAKE_WIDTH)
+			y = randint(0+SNAKE_HEIGHT, GAME_HEIGHT-SNAKE_WIDTH)
+			self.apple_pos.x, self.apple_pos.y = x,y
+			self.factory.connections["client1"].transport.write(self.apple.pos)
+			self.factory.connections["client2"].transport.write(self.apple.pos)
+		else:
+			# Send player2 new position of player1
+			print self.factory.connections[self], data
+			self.factory.connections["client2"].transport.write(data)
+
+
+class ClientConnFactory2(Factory):
+	def __init__(self, connections, client2):
+		self.connections = connections
+		self.client2 = client2
+	def buildProtocol(self, addr):
+		return ClientConnProtocol2(self)
+
+class ClientConnProtocol2(Protocol):
+	def __init__(self, factory):
+		self.factory = factory
+
+	def connectionMade(self):
+		self.factory.client2 = 1
+		self.factory.connections["client2"] = self
+		self.factory.connections[self] = "client2"
+		self.factory.connections["client2"].transport.write("Player 2 has entered the game.")
+		self.factory.connections["client1"].transport.write("Player 2 has entered the game.")
+		
+	def connectionLost(self, reason):
+		del self.factory.connections["client2"]
+		print "Player 2 has left the game."
+		#print "Lost connection:", reason
+
+	def dataReceived(self, data):
+		if data == "apple":
+			x = randint(0+SNAKE_WIDTH, GAME_WIDTH-SNAKE_WIDTH)
+			y = randint(0+SNAKE_HEIGHT, GAME_HEIGHT-SNAKE_WIDTH)
+			self.apple_pos.x, self.apple_pos.y = x,y
+			self.factory.connections["client1"].transport.write(self.apple.pos)
+			self.factory.connections["client2"].transport.write(self.apple.pos)
+		else:
+			# Send player1 new position of player2	
+			print self.factory.connections[self], data
+			self.factory.connections["client1"].transport.write(data)
 
 
 if __name__ == '__main__':
-	self.server = Server()
-	self.server.listen()
+	server = Server()
+	server.listen()
 	reactor.run()
